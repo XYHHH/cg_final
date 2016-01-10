a85de0f7b6e5fc889c14d9c818a743f91ebc287a
XYHHH <yuhongxu@epiclouds.net>
2016-01-10 16:15:51 +0800

    add simple light

--- cg_final/main.cpp
+++ cg_final/main.cpp
@@ -22,6 +22,26 @@ extern double viewAngle;
 
 bool gameStart = true;
 
+void lightInit()
+{	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 10.0, 1.0 };
+	GLfloat white_light[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat Light_Model_Ambient[] = { 0.2, 0.2, 0.2, 1.0 }; //
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Light_Model_Ambient);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -77,8 +97,7 @@ int main(int argc, char *argv[])
 	glutSpecialFunc(specialKeys);
 	glutSpecialUpFunc(upSpecialKeys);
 
-	cout << windowWidth << endl;
-	cout << windowHeight << endl;
+	lightInit();
 	glutSetCursor(GLUT_CURSOR_NONE);
 	SetCursorPos(windowWidth/2, windowHeight/2);
 	glutPassiveMotionFunc(OnMouseMove);
